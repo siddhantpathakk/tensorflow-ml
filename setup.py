8464bf8c68255ed56f919b3beab03b339307e522
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-08-01 22:39:17 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='tensorflow_ml',
+      version='0.1',
+      description='Testing installation of Package',
+      url='#',
+      author='Siddhant Pathak',
+      author_email='siddhantpathak2@gmail.com',
+      license='MIT',
+      packages=['mypackage'],
+      zip_safe=False)
