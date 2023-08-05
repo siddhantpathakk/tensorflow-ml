01ae459c39dcfdddb61a22bb241bb7281ddee9c9
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-08-05 16:37:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
-setup(name='tensorflow_ml',
-      version='0.1',
-      description='Testing installation of Package',
-      url='#',
-      author='Siddhant Pathak',
-      author_email='siddhantpathak2@gmail.com',
-      license='MIT',
-      packages=['mypackage'],
-      zip_safe=False)
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2' 
+DESCRIPTION = 'Tensorflow ML'
+LONG_DESCRIPTION = 'An abstract implementation of commonly used machine learning algorithms using TensorFlow 2.0'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="tensorflow_ml", 
+        version=VERSION,
+        author="Siddhant Pathak",
+        author_email="siddhantpathak2@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'tensorflow', 'ml'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
