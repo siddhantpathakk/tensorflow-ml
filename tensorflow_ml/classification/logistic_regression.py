7be99cc7e38470bd237715d6994be2e09256392e
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-08-07 00:46:00 +0800

    Logistic Reg implementation improved

--- tensorflow_ml/classification/logistic_regression.py
+++ tensorflow_ml/classification/logistic_regression.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 class LogisticRegression:
     def __init__(self):
         self.learning_rate = None
@@ -9,57 +8,58 @@ class LogisticRegression:
         self.batch_size = None
         self.reg_strength = None
         self.coefficients = None
-        self.verbose = True
+        self.verbose = False
         self.regularization = None
+        self.early_stopping_patience = None
 
-    def set_params(self, params : dict):
+    def set_params(self, params: dict):
         self.learning_rate = params['learning_rate']
         self.num_epochs = params['num_epochs']
         self.reg_strength = params['reg_strength']
         self.batch_size = params['batch_size']
         self.early_stopping_patience = params['early_stopping_patience']
         self.regularization = params["regularization"]
-        
-        self.learning_rate_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=self.learning_rate,
-                                                                                      decay_steps=1000,
-                                                                                      decay_rate=0.96,
-                                                                                      staircase=True)
-        
-    def get_params(self) -> dict:
 
-        return {"learning_rate": self.learning_rate, 
-                "num_epochs": self.num_epochs, 
-                "reg_strength": self.reg_strength,
-                "batch_size": self.batch_size,
-                "early_stopping_patience": self.early_stopping_patience}
-         
-       
+        self.learning_rate_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=self.learning_rate,
+            decay_steps=1000,
+            decay_rate=0.96,
+            staircase=True
+        )
+
+    def get_params(self) -> dict:
+        return {
+            "learning_rate": self.learning_rate,
+            "num_epochs": self.num_epochs,
+            "reg_strength": self.reg_strength,
+            "batch_size": self.batch_size,
+            "early_stopping_patience": self.early_stopping_patience
+        }
+
     def _scale_features(self, X):
-        # Mean normalization (subtract mean and divide by standard deviation)
-        # mean = np.mean(X, axis=0)
-        # std = np.std(X, axis=0)
-        # scaled_X = (X - mean) / std
-        # return scaled_X
-        return X
-        
+        mean = np.mean(X, axis=0)
+        std = np.std(X, axis=0)
+        scaled_X = (X - mean) / std
+        return scaled_X
+
     def sigmoid(self, z):
         return 1 / (1 + tf.exp(-z))
-    
+
     def softmax(self, z):
         return tf.nn.softmax(z)
 
-    def train_val_split(self, X, y, test_size=0.2, random_state=None):
+    def train_test_split(self, X, y, test_size=0.2, random_state=None):
         num_samples = X.shape[0]
         if random_state:
             np.random.seed(random_state)
         indices = np.arange(num_samples)
         np.random.shuffle(indices)
-        num_val_samples = int(test_size * num_samples)
-        val_indices = indices[:num_val_samples]
-        train_indices = indices[num_val_samples:]
-        X_train, X_val = X[train_indices], X[val_indices]
-        y_train, y_val = y[train_indices], y[val_indices]
-        return X_train, X_val, y_train, y_val
+        num_test_samples = int(test_size * num_samples)
+        test_indices = indices[:num_test_samples]
+        train_indices = indices[num_test_samples:]
+        X_train, X_test = X[train_indices], X[test_indices]
+        y_train, y_test = y[train_indices], y[test_indices]
+        return X_train, X_test, y_train, y_test
 
     def _regularization_term(self, coefficients):
         if self.regularization == 'l1':
@@ -68,97 +68,105 @@ class LogisticRegression:
             return self.reg_strength * tf.reduce_sum(tf.square(coefficients))
         else:
             return 0
+    def fit(self, X, y, X_val=None, y_val=None):
+        num_samples, num_features = X.shape
 
-    def fit(self, X, y):
-        X_scaled = self._scale_features(X)
-        X_scaled = np.hstack((np.ones((X_scaled.shape[0], 1)), X_scaled))
+        X = np.hstack((np.ones((num_samples, 1)), X))  # Add a bias term (1) to the scaled features
 
-        X_train, X_val, y_train, y_val = self.train_val_split(X_scaled, y, test_size=0.2, random_state=42)
+        # Convert numpy arrays to TensorFlow tensors
+        X = tf.constant(X, dtype=tf.float32)
+        y = tf.constant(y.reshape(-1, 1), dtype=tf.float32)
 
-        X_train, X_val = X_train.astype(np.float32), X_val.astype(np.float32)
+        # Calculate class weights for data imbalance
+        class_weights = self._calculate_class_weights(y)
 
-        num_classes = len(np.unique(y))
-        num_features = X_scaled.shape[1]
+        # Initialize the coefficients (weights) randomly
+        self.coefficients = tf.Variable(tf.random.normal((num_features + 1, 1), stddev=0.01))
 
-        self.coefficients = []
-        for class_label in range(num_classes):
-            y_binary = (y_train == class_label).astype(np.float32)
+        # Learning Rate Scheduling with momentum-based optimizer (Adam)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-            train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_binary)).shuffle(len(X_train)).batch(self.batch_size)
-            val_dataset = tf.data.Dataset.from_tensor_slices((X_val, (y_val == class_label).astype(np.float32))).batch(self.batch_size)
+        # Training loop with Mini-batch Gradient Descent and early stopping
+        num_batches = int(np.ceil(num_samples / self.batch_size))
+        best_val_loss = float('inf')
+        patience_count = 0
+        for epoch in range(self.num_epochs):
+            indices = np.arange(num_samples)
+            np.random.shuffle(indices)
 
+            for batch in range(num_batches):
+                start_idx = batch * self.batch_size
+                end_idx = min((batch + 1) * self.batch_size, num_samples)
+                batch_indices = indices[start_idx:end_idx]
 
-            # Initialize the coefficients with Glorot uniform initialization
-            coefficients_class = tf.Variable(tf.keras.initializers.GlorotUniform()((num_features, 1)))
+                batch_X = tf.gather(X, batch_indices)
+                batch_y = tf.gather(y, batch_indices)
 
-            optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate,clipvalue=1.0)
+                with tf.GradientTape() as tape:
+                    logits = tf.matmul(batch_X, self.coefficients)
+                    predictions = self.sigmoid(logits)
+                    loss = -tf.reduce_mean(class_weights * batch_y * tf.math.log(predictions) + class_weights * (1 - batch_y) * tf.math.log(1 - predictions))
+                    if self.reg_strength > 0.0:
+                        loss += self.reg_strength * tf.reduce_sum(tf.square(self.coefficients))
 
-            best_val_loss = float('inf')
-            early_stopping_count = 0
+                gradients = tape.gradient(loss, self.coefficients)
+                optimizer.apply_gradients(zip([gradients], [self.coefficients]))
 
-            for epoch in range(self.num_epochs):
-                train_loss = 0
-                for batch_X, batch_y in train_dataset:
-                    with tf.GradientTape() as tape:
-                        z = tf.matmul(batch_X, coefficients_class)
-                        probabilities = self.sigmoid(z)
-                        loss = -tf.reduce_mean(batch_y * tf.math.log(probabilities) + (1 - batch_y) * tf.math.log(1 - probabilities))
-                        loss += self._regularization_term(coefficients_class)
-
-                    gradients = tape.gradient(loss, coefficients_class)
-                    optimizer.apply_gradients(zip([gradients], [coefficients_class]))
-
-                    train_loss += loss
-
-                train_loss /= len(train_dataset)
-
-                val_loss = 0
-                for batch_X, batch_y in val_dataset:
-                    z = tf.matmul(batch_X, coefficients_class)
-                    probabilities = self.sigmoid(z)
-                    loss = -tf.reduce_mean(batch_y * tf.math.log(probabilities) + (1 - batch_y) * tf.math.log(1 - probabilities))
-                    val_loss += loss
-
-                val_loss /= len(val_dataset)
-
-                if val_loss < best_val_loss:
+            # Calculate validation loss and perform early stopping with patience
+            if X_val is not None and y_val is not None:
+                val_loss = self.evaluate(X_val, y_val)
+                if val_loss + self.tolerance < best_val_loss:
                     best_val_loss = val_loss
-                    early_stopping_count = 0
+                    patience_count = 0
                 else:
-                    early_stopping_count += 1
+                    patience_count += 1
+                    if patience_count >= self.patience:
+                        break  # Stop training if validation loss doesn't improve for 'patience' epochs
 
-                if early_stopping_count >= self.early_stopping_patience:
-                    print(f"Class {class_label}: Early stopping at epoch {epoch}")
-                    break
+            if self.verbose:
+                if X_val is None or y_val is None:
+                    print(f"Epoch {epoch + 1}/{self.num_epochs} - Train Loss: {loss:.5f}")
+                else:
+                    print(f"Epoch {epoch + 1}/{self.num_epochs} - Train Loss: {loss:.5f} - Val Loss: {val_loss:.5f}")
 
-                # print(f"Class {class_label}: Epoch {epoch+1}/{self.num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
-
-            self.coefficients.append(coefficients_class)
-            
+    def _calculate_class_weights(self, y):
+        unique_classes, class_counts = np.unique(y, return_counts=True)
+        total_samples = y.shape[0]
+        class_weights = total_samples / (len(unique_classes) * class_counts)
+        max_weight = np.max(class_weights)
+        class_weights = max_weight / class_weights
+        class_weights = tf.constant(class_weights, dtype=tf.float32)
+        return class_weights
+        
     def predict_proba(self, X):
         X_scaled = self._scale_features(X)
         X_scaled = np.hstack((np.ones((X_scaled.shape[0], 1)), X_scaled))
         X_scaled = tf.constant(X_scaled, dtype=tf.float32)
 
-        logits = [tf.matmul(X_scaled, coeff).numpy() for coeff in self.coefficients]
-        probabilities = [self.softmax(logit) for logit in logits]
-        return np.array(probabilities)
+        logits = tf.matmul(X_scaled, self.coefficients)  # Use matrix multiplication directly
+        probabilities = self.sigmoid(logits)
+        return probabilities.numpy()
 
     def predict(self, X):
         probabilities = self.predict_proba(X)
         return np.argmax(probabilities, axis=1)
 
     def score(self, X, y):
-        y_pred = self.predict(X)
-        accuracy = np.mean(y_pred == y)
+        y_pred_proba = self.predict_proba(X)
+
+        # Clip probabilities to avoid log(0) and log(1) issues
+        y_pred_proba = np.clip(y_pred_proba, 1e-7, 1 - 1e-7)
 
         y_binary = np.array([np.array([1.0 if i == label else 0.0 for i in range(len(np.unique(y)))]) for label in y])
-        y_pred_proba = self.predict_proba(X)
-        loss = -np.mean(y_binary * np.log(y_pred_proba) + (1 - y_binary) * np.log(1 - y_pred_proba))
+
+        # Use TensorFlow's binary cross-entropy loss function
+        loss = tf.keras.losses.binary_crossentropy(y_binary, y_pred_proba).numpy()
+        loss = np.mean(loss)
+
+        y_pred = np.argmax(y_pred_proba, axis=1)
+        accuracy = np.mean(y_pred == y)
 
         return accuracy, loss
 
     def evaluate(self, X, y):
-        y_pred = self.predict_proba(X)
-        loss = -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
-        return loss
+        return self.score(X, y)
